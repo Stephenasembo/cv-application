3f06622ca99ceae7d3fe88b8683e6dbdd8129fa4
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-09 19:19:49 +0300

    Feat: Create Person Object For User
    Allow for state sharing between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
+import { useState } from 'react';
 import PersonalInfo from './components/personalInfo';
 import Education from './components/education';
 import Experience from './components/experience';
+import GenerateCV from './components/generateCV';
 
 function App() {
+  const [person, setPerson] = useState({
+    name: '',
+    phone: '',
+    email: '',
+  });
+
   return (
     <>
-      <PersonalInfo />
+      <PersonalInfo infoObj={person} setInfoObj={setPerson} />
       <Education />
       <Experience />
+      <GenerateCV person={person} />
     </>
   )
 }
--- src/components/generateCV.jsx
+++ src/components/generateCV.jsx
@@ -1,8 +1,9 @@
-export default function GenerateCV({name}) {
-  console.log(name);
+export default function GenerateCV({person}) {
   return (
     <div className="generatedCV">
-      <h1>{name}</h1>
+      <h1>{person.name}</h1>
+      {person.phone !== '' && <p>Your phone number is: {person.phone}</p>}
+      {person.email !== '' && <p>Your email address is: {person.email}</p>}
     </div>
   )
 }
