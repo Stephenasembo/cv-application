6793ad92d236b38b62840b9344c85a4534a24f8b
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-04-09 19:17:25 +0300

    Feat: Update Display On Input Change
    Reflecting personal details change to the display

--- src/components/personalInfo.jsx
+++ src/components/personalInfo.jsx
@@ -1,17 +1,29 @@
-export default function PersonalInfo() {
+export default function PersonalInfo({infoObj, setInfoObj}) {
+  function updateName(newName) {
+    setInfoObj({...infoObj, name: newName});
+  }
+
+  function updatePhone(newPhone) {
+    setInfoObj({...infoObj, phone: newPhone});
+  }
+
+  function updateEmail(newEmail) {
+    setInfoObj({...infoObj, email: newEmail});
+  }
+
   return (
     <section className='personalInfo'>
       <label>
         Name: 
-        <input type='text'/>
+        <input type='text' onChange={(e) => updateName(e.target.value)}/>
       </label>
       <label>
         Phone Number: 
-        <input type='tel'/>
+        <input type='tel'onChange={(e) => updatePhone(e.target.value)}/>
       </label>
       <label>
         Email Address: 
-        <input type='email'/>
+        <input type='email' onChange={(e) => updateEmail(e.target.value)}/>
       </label>
     </section>
   )
